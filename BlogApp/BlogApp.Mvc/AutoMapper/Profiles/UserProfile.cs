c73711e6743fe1acedc9268a5d9019549581dd7d
burak bugra bekiroglu <bkrgl28@outlook.com>
2022-03-03 16:29:01 +0300

    User automapper configuration

---
+++ BlogApp/BlogApp.Mvc/AutoMapper/Profiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogApp.Entities.Concrete;
+using BlogApp.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Mvc.AutoMapper.Profiles
+{
+    public class UserProfile:Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserAddDto, User>();
+        }
+
+    }
+}
