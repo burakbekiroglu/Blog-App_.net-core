845e7263dec04751ded09aeb99c07326defd59f8
burak bugra bekiroglu <bkrgl28@outlook.com>
2022-03-11 18:26:10 +0300

    controller protected

--- BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace BlogApp.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
+    [Authorize(Roles = "Admin,Editor")]
     public class HomeController : Controller
     {
         public IActionResult Index()
--- BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/UserController.cs
+++ BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -1,24 +1,22 @@
-﻿using BlogApp.Entities.Concrete;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlogApp.Entities.Concrete;
 using BlogApp.Entities.Dtos;
-using BlogApp.Shared.Utilities.Results.ComplexTypes;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
 using BlogApp.Shared.Utilities.Extensions;
-using AutoMapper;
+using BlogApp.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.IO;
 using System.Text.Json;
-using BlogApp.Mvc.Areas.Admin.Models;
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using BlogApp.Mvc.Areas.Admin.Models;
 
-namespace BlogApp.Mvc.Areas.Admin.Controllers
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
@@ -28,26 +26,26 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
         private readonly IWebHostEnvironment _env;
         private readonly IMapper _mapper;
 
-        public UserController(UserManager<User> userManager, IWebHostEnvironment env, IMapper mapper,SignInManager<User> signInManager)
+        public UserController(UserManager<User> userManager, IWebHostEnvironment env, IMapper mapper, SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _env = env;
             _mapper = mapper;
             _signInManager = signInManager;
         }
-        [Authorize]
-        public async  Task<IActionResult> Index()
+        [Authorize(Roles="Admin")]
+        public async Task<IActionResult> Index()
         {
             var users = await _userManager.Users.ToListAsync();
-            return View(new UserListDto {
-            Users=users,
-            ResultStatus=ResultStatus.Success,
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
             });
         }
         [HttpGet]
         public IActionResult Login()
         {
-
             return View("UserLogin");
         }
         [HttpPost]
@@ -58,20 +56,21 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                 var user = await _userManager.FindByEmailAsync(userLoginDto.Email);
                 if (user!=null)
                 {
-                    var result = await _signInManager.PasswordSignInAsync(user, userLoginDto.Password, userLoginDto.RememberMe, false);
+                    var result = await _signInManager.PasswordSignInAsync(user, userLoginDto.Password,
+                        userLoginDto.RememberMe, false);
                     if (result.Succeeded)
                     {
-                        return RedirectToAction("Index","Home");
+                        return RedirectToAction("Index", "Home");
                     }
                     else
                     {
-                        ModelState.AddModelError("","E-posta adresiniz veya şifreniz yanlıştır");
+                        ModelState.AddModelError("","E-posta adresiniz veya şifreniz yanlıştır.");
                         return View("UserLogin");
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError("", "E-posta adresiniz veya şifreniz yanlıştır");
+                    ModelState.AddModelError("", "E-posta adresiniz veya şifreniz yanlıştır.");
                     return View("UserLogin");
                 }
             }
@@ -81,51 +80,47 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
             }
             
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpGet]
-        [Authorize]
         public async Task<JsonResult> GetAllUsers()
         {
             var users = await _userManager.Users.ToListAsync();
-            var userListDto = JsonSerializer.Serialize(new UserListDto { 
-            Users=users,
-            ResultStatus=ResultStatus.Success
-            },new JsonSerializerOptions {
-            ReferenceHandler=ReferenceHandler.Preserve
+            var userListDto =JsonSerializer.Serialize(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            },new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
             });
-
-
             return Json(userListDto);
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpGet]
-        [Authorize]
         public IActionResult Add()
         {
             return PartialView("_UserAddPartial");
         }
+        [Authorize(Roles = "Admin")]
         [HttpPost]
-        public async  Task<IActionResult> Add(UserAddDto userAddDto)
+        public async Task<IActionResult> Add(UserAddDto userAddDto)
         {
             if (ModelState.IsValid)
             {
                 userAddDto.Picture = await ImageUpload(userAddDto.UserName,userAddDto.PictureFile);
-               var user= _mapper.Map<User>(userAddDto);
-
-                var result = await _userManager.CreateAsync(user,userAddDto.Password);
-
-
+                var user = _mapper.Map<User>(userAddDto);
+                var result = await _userManager.CreateAsync(user, userAddDto.Password);
                 if (result.Succeeded)
                 {
-                    var userAddAjaxModel = JsonSerializer.Serialize(new UserAddAjaxViewModel {
-                    
-                        UserDto= new UserDto
+                    var userAddAjaxModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
+                    {
+                        UserDto = new UserDto
                         {
-                            ResultStatus=ResultStatus.Success,
-                            Message=$"{user.UserName} adlı kullanıcı adına sahip, kullanıcı başarıyla eklenmiştir. ",
-                            User=user
+                            ResultStatus = ResultStatus.Success,
+                            Message = $"{user.UserName} adlı kullanıcı adına sahip, kullanıcı başarıyla eklenmiştir.",
+                            User = user
                         },
-                        UserAddPartial= await this.RenderViewToStringAsync("_UserAddPartial",userAddDto)
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
                     });
                     return Json(userAddAjaxModel);
                 }
@@ -135,41 +130,36 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                     {
                         ModelState.AddModelError("",error.Description);
                     }
+
                     var userAddAjaxErrorModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
                     {
-
                         UserAddDto = userAddDto,
-                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial",userAddDto)
+                        UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
                     });
-
                     return Json(userAddAjaxErrorModel);
                 }
+                
             }
             var userAddAjaxModelStateErrorModel = JsonSerializer.Serialize(new UserAddAjaxViewModel
             {
-
                 UserAddDto = userAddDto,
                 UserAddPartial = await this.RenderViewToStringAsync("_UserAddPartial", userAddDto)
             });
-
             return Json(userAddAjaxModelStateErrorModel);
 
-
-
-
         }
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public async Task<JsonResult> Delete(int userId)
         {
             var user = await _userManager.FindByIdAsync(userId.ToString());
             var result = await _userManager.DeleteAsync(user);
             if (result.Succeeded)
             {
-                var deletedUser = JsonSerializer.Serialize(new UserDto {
-                    ResultStatus=ResultStatus.Success,
-                    Message=$"{user.UserName} adlı kullanıcı adına sahip kullanıcı başarıyla silinmiştir.",
-                    User=user
-                   
+                var deletedUser = JsonSerializer.Serialize(new UserDto
+                {
+                    ResultStatus = ResultStatus.Success,
+                    Message = $"{user.UserName} adlı kullanıcı adına sahip kullanıcı başarıyla silinmiştir.",
+                    User = user
                 });
                 return Json(deletedUser);
             }
@@ -178,26 +168,28 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                 string errorMessages = String.Empty;
                 foreach (var error in result.Errors)
                 {
-                  errorMessages=  $"*{error.Description}\n";
+                    errorMessages = $"*{error.Description}\n";
                 }
-                var deletedUserErrorModel = JsonSerializer.Serialize(new UserDto {
-                
-                    ResultStatus=ResultStatus.Error,
-                    Message= $"{user.UserName} adlı kullanıcı adına sahip kullanıcı silinirken bazı hatalar oluştu.\n{errorMessages}",
-                    User=user
+
+                var deletedUserErrorModel = JsonSerializer.Serialize(new UserDto
+                {
+                    ResultStatus = ResultStatus.Error,
+                    Message =
+                        $"{user.UserName} adlı kullanıcı adına sahip kullanıcı silinirken bazı hatalar oluştu.\n{errorMessages}",
+                    User = user
                 });
                 return Json(deletedUserErrorModel);
             }
         }
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<PartialViewResult> Update(int userId)
         {
-            var user = await _userManager.Users.FirstOrDefaultAsync(u=>u.Id==userId);
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == userId);
             var userUpdateDto = _mapper.Map<UserUpdateDto>(user);
             return PartialView("_UserUpdatePartial", userUpdateDto);
         }
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<IActionResult> Update(UserUpdateDto userUpdateDto)
         {
@@ -208,10 +200,11 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                 var oldUserPicture = oldUser.Picture;
                 if (userUpdateDto.PictureFile!=null)
                 {
-                    userUpdateDto.Picture =await ImageUpload(userUpdateDto.UserName, userUpdateDto.PictureFile);
+                    userUpdateDto.Picture = await ImageUpload(userUpdateDto.UserName, userUpdateDto.PictureFile);
                     isNewPictureUploaded = true;
                 }
-                var updatedUser = _mapper.Map<UserUpdateDto, User>(userUpdateDto,oldUser);
+
+                var updatedUser = _mapper.Map<UserUpdateDto, User>(userUpdateDto, oldUser);
                 var result = await _userManager.UpdateAsync(updatedUser);
                 if (result.Succeeded)
                 {
@@ -219,16 +212,16 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                     {
                         ImageDelete(oldUserPicture);
                     }
-                    var userUpdateViewModel = JsonSerializer.Serialize(new UserUpdateAjaxViewModel {
-                    
-                        UserDto=new UserDto
+
+                    var userUpdateViewModel = JsonSerializer.Serialize(new UserUpdateAjaxViewModel
+                    {
+                        UserDto = new UserDto
                         {
-                            ResultStatus=ResultStatus.Success,
-                            Message=$"{updatedUser.UserName} adlı kullanıcı başarıyla güncellenmiştir.",
-                            User=updatedUser
+                            ResultStatus = ResultStatus.Success,
+                            Message = $"{updatedUser.UserName} adlı kullanıcı başarıyla güncellenmiştir.",
+                            User = updatedUser
                         },
-                        UserUpdatePartial= await this.RenderViewToStringAsync("_UserUpdatePartial",userUpdateDto)
-                    
+                        UserUpdatePartial = await this.RenderViewToStringAsync("_UserUpdatePartial", userUpdateDto)
                     });
                     return Json(userUpdateViewModel);
                 }
@@ -238,54 +231,47 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
                     {
                         ModelState.AddModelError("",error.Description);
                     }
-                    var userUpdateErrorViewModel = JsonSerializer.Serialize(new UserUpdateAjaxViewModel
+                    var userUpdateErorViewModel = JsonSerializer.Serialize(new UserUpdateAjaxViewModel
                     {
-
-                        UserUpdateDto=userUpdateDto,
+                        UserUpdateDto = userUpdateDto,
                         UserUpdatePartial = await this.RenderViewToStringAsync("_UserUpdatePartial", userUpdateDto)
-
                     });
-                    return Json(userUpdateErrorViewModel);
+                    return Json(userUpdateErorViewModel);
                 }
+
             }
             else
             {
                 var userUpdateModelStateErrorViewModel = JsonSerializer.Serialize(new UserUpdateAjaxViewModel
                 {
-
                     UserUpdateDto = userUpdateDto,
                     UserUpdatePartial = await this.RenderViewToStringAsync("_UserUpdatePartial", userUpdateDto)
-
                 });
                 return Json(userUpdateModelStateErrorViewModel);
             }
-
         }
-        [Authorize]
-        public async Task<string> ImageUpload(string userName,IFormFile pictureFile)
+        [Authorize(Roles = "Admin,Editor")]
+        public async Task<string> ImageUpload(string userName, IFormFile pictureFile)
         {
+            
             string wwwroot = _env.WebRootPath;
-           // string fileName = Path.GetFileNameWithoutExtension(userAddDto.Picture.FileName);
+            
             string fileExtension = Path.GetExtension(pictureFile.FileName);
             DateTime dateTime = DateTime.Now;
             string fileName = $"{userName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
-            var path = Path.Combine($"{wwwroot}/img",fileName);
+            var path = Path.Combine($"{wwwroot}/img", fileName);
             await using (var stream = new FileStream(path, FileMode.Create))
             {
                 await pictureFile.CopyToAsync(stream);
             }
 
-            return fileName;
+            return fileName; 
         }
-
-
-        [Authorize]
+        [Authorize(Roles = "Admin,Editor")]
         public bool ImageDelete(string pictureName)
         {
             string wwwroot = _env.WebRootPath;
-
-            var fileToDelete = Path.Combine($"{wwwroot}/img",pictureName);
-
+            var fileToDelete = Path.Combine($"{wwwroot}/img", pictureName);
             if (System.IO.File.Exists(fileToDelete))
             {
                 System.IO.File.Delete(fileToDelete);
@@ -295,6 +281,14 @@ namespace BlogApp.Mvc.Areas.Admin.Controllers
             {
                 return false;
             }
+        }
+
+
+        [HttpGet]
+        public ViewResult AccessDenied()
+        {
+            return View();
+
         }
     }
 }
