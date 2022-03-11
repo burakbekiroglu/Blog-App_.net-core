260be62f3d002e33050deccee1143f11707c307a
burak bugra bekiroglu <bkrgl28@outlook.com>
2022-03-11 19:07:37 +0300

    ViewComponent refactoring

---
+++ BlogApp/BlogApp.Mvc/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Mvc.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+    }
+}
---
+++ BlogApp/BlogApp.Mvc/Areas/Admin/ViewComponents/UserMenuViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using BlogApp.Entities.Concrete;
+using BlogApp.Mvc.Areas.Admin.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Mvc.Areas.Admin.ViewComponents
+{
+    public class UserMenuViewComponent:ViewComponent
+    {
+
+        private readonly UserManager<User> _userManager;
+
+        public UserMenuViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public ViewViewComponentResult Invoke()
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+
+
+            return View(new UserViewModel {
+            User=user
+            });
+        }
+    }
+}
